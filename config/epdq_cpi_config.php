0f540a397efc159ba60f7a57897b6fae12fa80b7
Ollie Rattue <orattue@toomanytabs.com>
2010-07-31 19:44:07 +0100

    Added config file and library class skeleton functions

---
+++ config/epdq_cpi_config.php
@@ -0,0 +1,14 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| ePDQ CPI library configuration file
+| -------------------------------------------------------------------------
+*/
+
+// Client specific details
+define('EPDQ_CLIENTID', '');
+define('EPDQ_PASSPHRASE', '');
+
+/* End of file epdq_cpi_config.php */
+/* Location: ./application/config/epdq_cpi_config.php */
